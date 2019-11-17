bf2dd6438ef829c4901fc21b30dfd174faa93cfb
tang_huibo <1215893264@qq.com>
2019-11-17 23:29:08 +0800

    完成基础布局

--- src/App.jsx
+++ src/App.jsx
@@ -1,75 +1,75 @@
-import React, { Component } from 'react';
-import { Table, Divider, Tag } from 'antd';
-import './App.css';
+import React, { Component } from "react";
+import { Typography, Input, Layout, Button, Switch, Icon } from "antd";
 
-const columns = [
-  {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name',
-    render: text => <div>{text}</div>,
-  },
-  {
-    title: 'Age',
-    dataIndex: 'age',
-    key: 'age',
-  },
-  {
-    title: 'Address',
-    dataIndex: 'address',
-    key: 'address',
-  },
-  {
-    title: 'Tags',
-    key: 'tags',
-    dataIndex: 'tags',
-    render: tags => (
-      <span>
-        {tags.map(tag => {
-          let color = tag.length > 5 ? 'geekblue' : 'green';
-          if (tag === 'loser') {
-            color = 'volcano';
-          }
-          return (
-            <Tag color={color} key={tag}>
-              {tag.toUpperCase()}
-            </Tag>
-          );
-        })}
-      </span>
-    ),
-  }
-];
+import style from "./app.module.css";
 
-const data = [
-  {
-    key: '1',
-    name: 'John Brown',
-    age: 32,
-    address: 'New York No. 1 Lake Park',
-    tags: ['nice', 'developer'],
-  },
-  {
-    key: '2',
-    name: 'Jim Green',
-    age: 42,
-    address: 'London No. 1 Lake Park',
-    tags: ['loser'],
-  },
-  {
-    key: '3',
-    name: 'Joe Black',
-    age: 32,
-    address: 'Sidney No. 1 Lake Park',
-    tags: ['cool', 'teacher'],
-  },
-];
-
+const { Header, Footer, Sider, Content } = Layout;
+const { Search } = Input;
+const { Title } = Typography;
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <Table columns={columns} dataSource={data} />
+        <Header>
+          <Title className={style.title} style={{ color: "#FFFFFF" }}>
+            power proxy
+          </Title>
+        </Header>
+        <Content className={style.content}>
+          <div>
+            <Search
+              size="large"
+              placeholder="输入关键词搜索"
+              onSearch={value => console.log(value)}
+              className={style.search}
+              style={{ width: 300 }}
+            />
+            <div className={style.headerButtonDiv}>
+              <Button
+                type="primary"
+                className={style.headerButton}
+                size="large"
+              >
+                新增
+              </Button>
+              <Button
+                type="primary"
+                className={style.headerButton}
+                size="large"
+              >
+                删除
+              </Button>
+              <Button
+                type="primary"
+                className={style.headerButton}
+                size="large"
+              >
+                导入
+              </Button>
+              <Button
+                type="primary"
+                className={style.headerButton}
+                size="large"
+              >
+                导出
+              </Button>
+            </div>
+
+            <div className={style.headerRightDiv}>
+              <Button size="large" type="link" style={{ paddingRight: 3 }}>
+                开启编辑模式
+              </Button>
+              <Switch
+                size="large"
+                checkedChildren={<Icon type="check" />}
+                unCheckedChildren={<Icon type="close" />}
+                defaultChecked
+              />
+            </div>
+          </div>
+
+          <Content>123</Content>
+        </Content>
       </div>
     );
   }
